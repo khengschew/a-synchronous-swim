05c5145c4f98c925c79dac593b1c2aefde1ca110
khengschew <khengc@gmail.com>
2019-01-22 11:10:54 -0800

    Implement button to issue random move to swimmers

--- client/js/keypressHandler.js
+++ client/js/keypressHandler.js
@@ -7,4 +7,13 @@ $('body').on('keydown', (event) => {
   }
 });
 
+var $button = $('<button>We Like To Move It Move It</button>').on('click', (e) => {
+  // Generate random direction
+  var directions = ['up', 'down', 'left', 'right'];
+
+  // Move in random direction
+  SwimTeam.move(directions[Math.floor(Math.random() * 4)]);
+});
+$('h1').after($button);
+
 console.log('Client is running in the browser!');
