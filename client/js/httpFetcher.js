68f254b0e0dea5f1c41edeb107fdbc78684bcdbc
khengschew <khengc@gmail.com>
2019-01-22 11:26:48 -0800

    Implement client Ajax call to server, server returns random direction

--- client/js/httpFetcher.js
+++ client/js/httpFetcher.js
@@ -1,5 +1,16 @@
 (function() {
 
-  const serverUrl = '?';
+  const serverUrl = 'http://127.0.0.1:3000';
+
+  $.ajax({
+    url: serverUrl,
+    type: 'GET',
+    success: (data) => (
+      console.log(data)
+    ),
+    error: (error) => (
+      console.error('Request failed', error)
+    )
+  });
 
 })();
--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -1,6 +1,16 @@
 const headers = require('./cors');
 
 module.exports = (req, res) => {
+  headers['Content-Type'] = 'application/json';
+
   res.writeHead(200, headers);
-  res.end();
+  console.log('the server is working!');
+
+  // Generate random direction
+  var directions = ['up', 'down', 'left', 'right'];
+
+  // Move in random direction
+  var newDirection = directions[Math.floor(Math.random() * 4)];
+
+  res.end(JSON.stringify({direction: newDirection}));
 };
