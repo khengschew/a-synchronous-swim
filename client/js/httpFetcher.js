ae2022aa9be46a21f0a53f3349da290a42fc56f4
khengschew <khengc@gmail.com>
2019-01-22 11:40:41 -0800

    Update button to trigger ajax call to retrieve random direction

--- client/js/httpFetcher.js
+++ client/js/httpFetcher.js
@@ -1,16 +1,14 @@
-(function() {
+var SwimServer = {
+  serverUrl: 'http://127.0.0.1:3000',
 
-  const serverUrl = 'http://127.0.0.1:3000';
-
-  $.ajax({
-    url: serverUrl,
-    type: 'GET',
-    success: (data) => (
-      console.log(data)
-    ),
-    error: (error) => (
-      console.error('Request failed', error)
-    )
-  });
-
-})();
+  getDirection: function(successCB, errorCB = null) {
+    $.ajax({
+      url: SwimServer.serverUrl,
+      type: 'GET',
+      success: successCB,
+      error: errorCB || function(error) {
+        console.error('Request failed', error);
+      }
+    });
+  }
+};
--- client/js/keypressHandler.js
+++ client/js/keypressHandler.js
@@ -7,13 +7,22 @@ $('body').on('keydown', (event) => {
   }
 });
 
-var $button = $('<button>We Like To Move It Move It</button>').on('click', (e) => {
-  // Generate random direction
-  var directions = ['up', 'down', 'left', 'right'];
+// var $button = $('<button>We Like To Move It Move It</button>').on('click', (e) => {
+//   // Generate random direction
+//   var directions = ['up', 'down', 'left', 'right'];
 
-  // Move in random direction
-  SwimTeam.move(directions[Math.floor(Math.random() * 4)]);
+//   // Move in random direction
+//   SwimTeam.move(directions[Math.floor(Math.random() * 4)]);
+// });
+// $('h1').after($button);
+
+var $asyncButton = $('<button>We Like To Move It Move It</button>').on('click', (e) => {
+  // Generate random direction from server
+  SwimServer.getDirection((data) => {
+    // Move in server direction
+    SwimTeam.move(data.direction);
+  });
 });
-$('h1').after($button);
+$('h1').after($asyncButton);
 
 console.log('Client is running in the browser!');
