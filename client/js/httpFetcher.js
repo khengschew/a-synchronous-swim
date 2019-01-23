2a5a0fae27ba25b648c623a340eb618eb952de6d
khengschew <khengc@gmail.com>
2019-01-22 16:04:52 -0800

    Update client to continue polling on a timeout

--- client/js/httpFetcher.js
+++ client/js/httpFetcher.js
@@ -1,3 +1,16 @@
+(function poll() {
+  fetch('http://127.0.0.1:3000', {
+    type: 'GET',
+    header: {'Content-Type': 'application/json'}
+  })
+    .then(res => res.json())
+    .then(res => {
+      SwimTeam.move(res.direction);
+      setTimeout(poll, 100);
+    })
+    .catch(error => console.error('Request failed', error));
+})();
+
 var SwimServer = {
   serverUrl: 'http://127.0.0.1:3000',
 
